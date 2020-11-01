514afcdfaf138ee6a0b0a6473d93e5476e9ed7cc
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2020-11-01 22:22:52 +0100

    Coded a small text for testing UI

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 Quarto AI player
 """
 from Core import QuartoGame
+from UI import QuartoUI
 
+UI = QuartoUI.QuartoUI()
 
-
+i = 1
+while True:
+    UI.update()
+    i = i+1
+    if i == 1e4:
+        break
