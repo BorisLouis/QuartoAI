9345b35c3d29588dedffc13cf6f2d26933ce5f2a
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2020-11-18 22:56:41 +0100

    Added a controller to link UI and game

---
+++ Controller/QuartoController.py
@@ -0,0 +1,13 @@
+from Core import QuartoGame
+from UI import QuartoUI
+import time
+
+def run():
+    while True:
+        UI = QuartoUI.QuartoUI()
+
+        game = QuartoGame.QuartoGame()
+        state = game.board
+        UI.update(state)
+        time.sleep(10)
+        break
