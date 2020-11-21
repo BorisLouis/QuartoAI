89ddec028c9ef094bd5c844cabc0cbb37de184e1
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2020-11-21 22:16:53 +0100

    tested UI

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 """
 Quarto AI player
 """
-
+import time
 
 from Controller import QuartoController
+from UI import QuartoUI
+
+#QuartoController.run()
+EMPTY = None
+state = [[EMPTY, EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY, EMPTY]]
 
 
-QuartoController.run()
+UI = QuartoUI.QuartoUI()
+while True:
+    UI.update(state,'POGU')
+    time.sleep(20)
+    break
